0b487fc98b88d3e8dc3c3ead4662e7632dd5e7dd
Will Bainbridge <http://cfd.direct>
2025-10-08 11:12:20 +0100

    isothermalFluid: Use p_rgh in transonic buoyant pressure equation

--- applications/modules/isothermalFluid/correctBuoyantPressure.C
+++ applications/modules/isothermalFluid/correctBuoyantPressure.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2022-2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2022-2025 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -147,7 +147,7 @@ void Foam::solvers::isothermalFluid::correctBuoyantPressure()
 
         while (pimple.correctNonOrthogonal())
         {
-            tp_rghEqn = p_rghDDtEqn - fvm::laplacian(rhorAAtUf, p);
+            tp_rghEqn = p_rghDDtEqn - fvm::laplacian(rhorAAtUf, p_rgh);
             fvScalarMatrix& p_rghEqn = tp_rghEqn.ref();
 
             // Relax the pressure equation to ensure diagonal-dominance
